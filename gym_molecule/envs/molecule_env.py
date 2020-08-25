1eb4c04176fbd079c181b5d3d0d59051cb02303a
ahnsoo002 <ahnsoo002@cs.uct.ac.za>
2020-08-25 16:12:35 +0200

    Implemented seed method

--- gym_molecule/envs/molecule_env.py
+++ gym_molecule/envs/molecule_env.py
@@ -1,11 +1,27 @@
 import gym
-from gym import error, spaces, utils
+from gym import spaces, utils
 from gym.utils import seeding
 
+from rdkit import Chem
+
+
+# TODO
+# 1. create action_space and observation_space
+# 2. define all env methods
+# 3. create the interface between env and agent
+# 4. placeholder methods for choosing an action and for calculating a reward
 
 class MoleculeEnvironment(gym.Env):
+    metadata = {'render.modes': ['human']}
+
     def __init__(self):
         super().__init__()
+
+        self.action_space = gym.spaces.MultiDiscrete(0,0)
+        self.observation_space = gym.spaces.MultiDiscrete(0,0)
+
+        self.seed()
+        self.reset()
 
     def step(self, action):
         print("step")
@@ -16,8 +32,9 @@ class MoleculeEnvironment(gym.Env):
     def render(self, mode='human', close=False):
         print("render")
 
-    def seed(self):
-        print("seed not set")
-
     def close(self):
         print("Closing application")
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
