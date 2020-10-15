71384d9286bcd59b7a849cf2e1e9cd8dd3a174c4
ahnsoo002 <ahnsoo002@cs.uct.ac.za>
2020-10-15 19:20:42 +0200

    Updated setup.py version, ipython requirement removed due to it being unused.

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='gym_molecule',
-      version='0.0.1',
+      version='1.0.0',
       description="A package for RL based drug molecule generation",
       author="Soo Kyung Ahn",
       author_email="sahnsookyung@gmail.com",
       packages=['gym_molecule'],
-      install_requires=['gym', 'numpy', 'ipython']
+      install_requires=['gym', 'numpy']
       )
